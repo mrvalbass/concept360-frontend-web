5a33754ec2cc4536942dffcbb6e5e3818679d6b2
Your name <Your email>
2024-05-20 17:33:33 +0200

    SignUp and SignIn for specialist, switch pages if connected

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
 import "@/styles/globals.css";
+import Head from "next/head";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import users from "../../reducers/users";
+
+const store = configureStore({
+  reducer: { users },
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Head>
+        <title>Concept 360</title>
+      </Head>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
---
+++ src/pages/login.js
@@ -0,0 +1,128 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { ChangeConnectionState } from "../../reducers/users";
+
+export default function login() {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.users.value);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [discipline, setDiscipline] = useState("");
+
+  const SignUp = async () => {
+    const form = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        password,
+        state: "specialist",
+        discipline,
+      }),
+    };
+    const response = await fetch(
+      "http://localhost:3000/users/signup",
+      form
+    ).then((r) => r.json());
+    dispatch(ChangeConnectionState());
+    router.push("/");
+  };
+
+  const signIn = async () => {
+    const form = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    };
+    const response = await fetch(
+      "http://localhost:3000/users/signin",
+      form
+    ).then((r) => r.json());
+    dispatch(ChangeConnectionState());
+    router.push("/");
+  };
+  return (
+    <main
+      className={`flex flex-col min-h-screen min-w-full items-center justify-center`}
+    >
+      <div>Concept 360</div>
+      <div className={"flex flex-col"}>
+        <div>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            value={email}
+            placeholder="adresse e-mail"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            value={password}
+            placeholder="Mot de passe"
+          />
+          <button
+            className="w-60 h-8 bg-white text-black rounded-2xl border-2 font-semibold"
+            onClick={signIn}
+          >
+            Connection
+          </button>
+        </div>
+        <div>
+          <input
+            onChange={(e) => setFirstName(e.target.value)}
+            type="text"
+            value={firstName}
+            placeholder="Prénom"
+          />
+          <input
+            onChange={(e) => setLastName(e.target.value)}
+            type="text"
+            value={lastName}
+            placeholder="Nom"
+          />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            value={email}
+            placeholder="adresse e-mail"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            value={password}
+            placeholder="Mot de passe"
+          />
+
+          <select
+            value={discipline}
+            onChange={(e) => setDiscipline(e.target.value)}
+          >
+            <option value={""} disabled>
+              Votre Spécialité ?
+            </option>
+            <option value={"kiné"}>Kiné</option>
+            <option value={"Ostéo"}>Ostéo</option>
+            <option value={"Psy"}>Psy</option>
+            <option value={"Coach"}>Coach</option>
+          </select>
+
+          <button
+            className="w-60 h-8 bg-white text-black rounded-2xl border-2 font-semibold"
+            onClick={SignUp}
+          >
+            Soumettre
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
