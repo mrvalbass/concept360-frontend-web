7e0f0f57de0886e9531f31c4cfc66decc790ab79
melanie-sr <melanie.saint.roma@gmail.com>
2024-05-22 16:43:41 +0200

    commit just in case : please ignore

--- src/components/Exercice.js
+++ src/components/Exercice.js
@@ -1,31 +1,38 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { addExerciceToStore } from "../reducers/exercices";
 // import { useRouter } from "next/router";
 
 export default function Exercice() {
-  const dispatch = useDispatch();
-  //   const router = useRouter();
-  //   const [exerciceData, setExerciceData] = useState(null);
-  const exercices = useSelector((state) => state.exercices.value);
-  const addExercice = (exerciceData) => {
-    dispatch(addExerciceToStore(exerciceData));
-  };
   // const handleDelete = ()= > {
   // }
+
+  const [exercices, setExercices] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:3000/exercices/exerciceList")
+      .then((response) => response.json())
+      .then((data) => setExercices(data));
+  }, []);
 
   //   if (!exerciceData) return <div className='border-2 grow'>No Data</div>;
 
   return (
     <div className='border-4 border-[green] flex flex-row space-x-20 p-5'>
-      {exercices}
-      <FontAwesomeIcon
-        className='text-2xl duration-75 hover:scale-110'
-        onClick={() => handleDelete()}
-        icon={faXmark}
-      />
+      <h1>Liste des Exercices</h1>
+      {exercices.map((exercice, index) => (
+        <div key={index}>
+          <p>{exercice.name}</p>
+          <p>{exercice.specialities}</p>
+          <p>{exercice.createdBy}</p>
+          <FontAwesomeIcon
+            className='text-2xl duration-75 hover:scale-110'
+            onClick={() => handleDelete()}
+            icon={faXmark}
+          />
+        </div>
+      ))}
     </div>
   );
 }
