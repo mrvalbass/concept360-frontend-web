ee41e541ddd3205c205dff9c384623d8c1c5ac4b
melanie-sr <melanie.saint.roma@gmail.com>
2024-05-23 10:08:46 +0200

    database page component + tailwinD

--- src/components/Exercice.js
+++ src/components/Exercice.js
@@ -1,36 +1,46 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { addExerciceToStore } from "../reducers/exercices";
+// import { useSelector } from "react-redux";
+// import { addExerciceToStore } from "../reducers/exercices";
 // import { useRouter } from "next/router";
 
 export default function Exercice() {
   // const handleDelete = ()= > {
   // }
 
-  const [exercices, setExercices] = useState(null);
+  const [exercices, setExercices] = useState([]);
   useEffect(() => {
     fetch("http://localhost:3000/exercices/exerciceList")
       .then((response) => response.json())
-      .then((data) => setExercices(data));
+      .then((data) => {
+        setExercices(data.exercices);
+      })
+
+      .catch((error) => console.error("Error fetching exercices:", error));
   }, []);
 
-  //   if (!exerciceData) return <div className='border-2 grow'>No Data</div>;
+  if (!exercices) {
+    return <div className='border-2 grow'>No Data</div>;
+  }
 
+  //space-x-20 p-5
   return (
-    <div className='border-4 border-[green] flex flex-row space-x-20 p-5'>
-      <h1>Liste des Exercices</h1>
+    <div className='w-[100%]'>
       {exercices.map((exercice, index) => (
-        <div key={index}>
-          <p>{exercice.name}</p>
-          <p>{exercice.specialities}</p>
-          <p>{exercice.createdBy}</p>
-          <FontAwesomeIcon
-            className='text-2xl duration-75 hover:scale-110'
-            onClick={() => handleDelete()}
-            icon={faXmark}
-          />
+        <div
+          key={index}
+          className='grid grid-cols-4 gap-4 place-content-between  p-2 m-3 border-b'>
+          <p className='flex items-center justify-start '>{exercice.title}</p>
+          <p className='flex items-center justify-start '>
+            {exercice.movement}
+          </p>
+          <p className='flex items-center justify-start '>
+            {exercice.bodyPart}
+          </p>
+          <p className='flex items-center justify-end  text-xl duration-75 hover:scale-110 text-[#067D5D]'>
+            <FontAwesomeIcon icon={faXmark} />
+          </p>
         </div>
       ))}
     </div>
--- src/pages/database.js
+++ src/pages/database.js
@@ -6,17 +6,19 @@ export default function Programs() {
     <>
       <Header />
       <main
-        className={`flex flex-col min-h-screen min-w-full items-center justify-center bg-[linear-gradient(149deg,_rgba(255,_255,_255,_0.50)_10%,_rgba(6,_125,_93,_0.50)_65%,_rgba(0,_165,_172,_0.50)_100%)]`}>
-        <div className='border-4 border-[red] bg-white ms-2.5	p-8	rounded'>
-          <h2 className='flex border-4 border-[yellow] items-center justify-center @apply rounded-[5px_5px_0px_0px] p-5'>
+        className={`flex flex-col min-h-screen min-w-full items-center justify-center bg-[linear-gradient(149deg,rgba(255,255,255,0.40)10%,rgba(6,125,93,0.40)65%,rgba(0,165,172,0.40)100%)]`}>
+        <div className='w-[40%] h-[40%] bg-white ms-2.5 p-8 rounded'>
+          <h2 className='font-medium text-xl font-[Sora] border-b flex items-center justify-center @apply rounded-[5px_5px_0px_0px] p-5'>
             Exercices
           </h2>
-          <div className='border-4 border-[blue]'>
-            <Exercice />
-            <Exercice />
+          <div className='p-2'>
             <Exercice />
           </div>
-          <button>Create new exercice</button>
+          <div className='flex items-center justify-center '>
+            <button className='flex bg-white drop-shadow-lg w-[40%] h-[40px] items-center justify-center font-semibold'>
+              Create new exercice
+            </button>
+          </div>
         </div>
       </main>
     </>
