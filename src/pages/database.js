aadfa053242789c055bb0670d7f7f03348e13fba
ValentinGuillot <97942568+mrvalbass@users.noreply.github.com>
2024-05-23 12:27:27 +0200

    Add Card Component

--- src/pages/database.js
+++ src/pages/database.js
@@ -1,11 +1,26 @@
 import Header from "@/components/Header";
 import Exercice from "@/components/Exercice";
-import DisplayRoutine from "@/components/DisplayRoutine";
+import Card from "@/components/Card";
 import RoutineModal from "@/components/RoutineModal";
-import { useState } from "react";
+import Routine from "@/components/Routine";
+import { useEffect, useState } from "react";
 
 export default function Programs() {
   const [openRoutineModal, setOpenRoutineModal] = useState(false);
+  const [routines, setRoutines] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const routinesData = await fetch("http://localhost:3000/routines/").then(
+        (r) => r.json()
+      );
+      setRoutines(routinesData.routines);
+    })();
+  }, []);
+
+  const routinesComponents =
+    routines && routines.map((routine, i) => <Routine key={i} {...routine} />);
+
   return (
     <>
       <RoutineModal
@@ -14,21 +29,28 @@ export default function Programs() {
       />
       <Header />
       <main
-        className={`flex justify-center p-10 min-h-[90vh] gap-10 bg-[linear-gradient(149deg,_rgba(255,_255,_255,_0.50)_10%,_rgba(6,_125,_93,_0.50)_65%,_rgba(0,_165,_172,_0.50)_100%)]`}>
-        <div className='w-[40%] h-[40%] bg-white ms-2.5 p-8 rounded'>
-          <h2 className='font-medium text-xl font-[Sora] border-b flex items-center justify-center @apply rounded-[5px_5px_0px_0px] p-5'>
+        className={`flex justify-center p-10 min-h-[90vh] gap-10 bg-[linear-gradient(149deg,_rgba(255,_255,_255,_0.50)_10%,_rgba(6,_125,_93,_0.50)_65%,_rgba(0,_165,_172,_0.50)_100%)]`}
+      >
+        <div className="w-[40%] h-[40%] bg-white ms-2.5 p-8 rounded">
+          <h2 className="font-medium text-xl font-[Sora] border-b flex items-center justify-center @apply rounded-[5px_5px_0px_0px] p-5">
             Exercices
           </h2>
-          <div className='p-2'>
+          <div className="p-2">
             <Exercice />
           </div>
-          <div className='flex items-center justify-center '>
-            <button className='flex bg-white drop-shadow-lg w-[40%] h-[40px] items-center justify-center font-semibold'>
+          <div className="flex items-center justify-center ">
+            <button className="flex bg-white drop-shadow-lg w-[40%] h-[40px] items-center justify-center font-semibold">
               Create new exercice
             </button>
           </div>
         </div>
-        <DisplayRoutine setOpenRoutineModal={setOpenRoutineModal} />
+        <Card
+          title="Routines"
+          displayButton
+          onButtonClick={setOpenRoutineModal}
+        >
+          {routinesComponents}
+        </Card>
       </main>
     </>
   );
