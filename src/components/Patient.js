3d1761812446e40ecb74f8ca0a3e3bd202595086
Your name <Your email>
2024-05-23 16:56:35 +0200

    work on page client

--- src/components/Patient.js
+++ src/components/Patient.js
@@ -1,11 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
 export default function Patient(props) {
   return (
     <>
-      <div className={"flex flex-row gap-5"} key={props.key}>
-        <div>firstName : {props.firstName}</div>
-        <div>lastName : {props.lastName}</div>
+      <div
+        className={
+          "grid grid-cols-4 gap-4 place-content-between  p-2 m-3 border-b"
+        }
+        key={props.key ? props.key : 0}
+      >
+        <div className="w-12 rounded-full border-4">
+          <Image
+            src="/gigachad.jpg"
+            width={565}
+            height={601}
+            alt="Patient Profile Picture"
+            className="rounded-full"
+          />
+        </div>
+        <p className="flex items-center justify-start ">
+          Prénom : {props.firstName}
+        </p>
+        <p className="flex items-center justify-start ">
+          Nom : {props.lastName}
+        </p>
         <FontAwesomeIcon
+          className="flex items-center justify-end  text-xl duration-75 hover:scale-110 text-[#067D5D]"
           onClick={() => props.function(props.patient)}
           icon={props.icon}
         />
--- src/pages/Clients.js
+++ src/pages/Clients.js
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
-import MyPatientList from "@/components/MyPatientList";
+import Card from "@/components/Card";
+import Patient from "@/components/Patient";
 
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import AllPatientList from "@/components/AllPatientList";
+
+import { faSquarePlus, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 export default function Clients() {
   const specialist = useSelector((state) => state.users.value);
@@ -17,6 +19,7 @@ export default function Clients() {
         "http://localhost:3000/users/state/patient"
       ).then((r) => r.json());
       setAllPatientData(allPatientData.Patient);
+
       const specialistData = await fetch(
         `http://localhost:3000/users/getPatientList/${specialist._id}`
       ).then((r) => r.json());
@@ -58,23 +61,48 @@ export default function Clients() {
     setReRender(!reRender);
   };
 
+  const allPatientFiltred = allPatientData.filter((patient) =>
+    patientInSpecialistList
+      ? !patientInSpecialistList.find((element) => patient._id === element._id)
+      : ""
+  );
+
+  const patientList = allPatientFiltred.map((patient, i) => {
+    return (
+      <Patient
+        key={i}
+        firstName={patient.user.firstName}
+        lastName={patient.user.lastName}
+        function={addToSpecialistList}
+        patient={patient}
+        icon={faSquarePlus}
+      />
+    );
+  });
+
+  const patientListInSpecialistList = patientInSpecialistList
+    .map((patient, i) => {
+      return (
+        <Patient
+          key={i}
+          firstName={patient.user.firstName}
+          lastName={patient.user.lastName}
+          function={deletePatientFromSpecialistList}
+          patient={patient}
+          icon={faTrashCan}
+        />
+      );
+    })
+    .reverse();
+
   return (
     <>
       <Header />
       <main
-        className={`flex flex-col gap-5 min-h-[90vh] p-5 items-center justify-center`}
+        className={`flex justify-center p-10 min-h-[90vh] gap-10 bg-[linear-gradient(149deg,_rgba(255,_255,_255,_0.50)_10%,_rgba(6,_125,_93,_0.50)_65%,_rgba(0,_165,_172,_0.50)_100%)]`}
       >
-        <div>Pages des Clients de Mr. {specialist.user.lastName}</div>
-        <div className={"flex flex-row min-w-full justify-between"}>
-          <AllPatientList
-            allPatientData={allPatientData}
-            addToSpecialistList={addToSpecialistList}
-          />
-          <MyPatientList
-            patientInSpecialistList={patientInSpecialistList}
-            deletePatientFromSpecialistList={deletePatientFromSpecialistList}
-          />
-        </div>
+        <Card title="Patients du Cabinet"> {patientList}</Card>
+        <Card title="Mes Patients"> {patientListInSpecialistList}</Card>
       </main>
     </>
   );
